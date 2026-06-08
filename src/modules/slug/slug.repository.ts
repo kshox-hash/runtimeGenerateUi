cffec23683b9ef063b212aa0c8adf0b75a3eb9bd
kshox❤️ <josepenacurihual@gmail.com>
2026-06-08 17:49:43 -0400

    ""

--- src/modules/slug/slug.repository.ts
+++ src/modules/slug/slug.repository.ts
@@ -8,7 +8,7 @@ export async function findSlugByUserIdRepository(userId: string) {
   const result = await pool.query(
     `
     SELECT *
-    FROM slugs
+    FROM public_slug
     WHERE user_id = $1
     LIMIT 1
     `,
