2fc55c6466e06c78ff5ef29b840da5146d898aa4
kshox❤️ <josepenacurihual@gmail.com>
2026-06-24 13:51:50 -0400

    comentarios

--- src/modules/menus/public-portal/public-portal.routes.ts
+++ src/modules/menus/public-portal/public-portal.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { publicPortalController } from "./public-portal.controller";
 import { quotesSubmitController } from "../../quotes/quotes.controller";
 import { portalSessionMiddleware } from "./portal-session.middleware";
+import { statisticsController } from "../../stadistics/stadistics.controller";
 
 const router = Router();
 
@@ -9,5 +10,6 @@ router.get("/shop/:publicSlug",           publicPortalController.open);
 router.get("/shop/:publicSlug/cotizador", publicPortalController.openQuotes);
 router.post("/shop/:publicSlug/quotes/submit", portalSessionMiddleware, quotesSubmitController.submit);
 router.post("/api/public/:publicSlug/reviews", publicPortalController.submitReview);
+router.get("/api/public/reviews/:userId", statisticsController.getPublicReviews);
 
 export default router;
